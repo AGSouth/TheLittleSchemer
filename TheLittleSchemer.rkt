17f837229ed154e3c4a57d349811cba6f1538d3c
Joe Maline <joe.maline@applegrovesouth.com>
2020-01-15 10:42:58 -0500

    Finished Programming Chapter 3

--- TheLittleSchemer.rkt
+++ TheLittleSchemer.rkt
@@ -14,6 +14,7 @@
     (cond ((null? l) #f)
           (else (or (eq? a (car l)) (member? a (cdr l)))))))
 
+; Chapter 3 The Littls Schemer
 
 (define rember
   (lambda (a lat)
@@ -39,3 +40,51 @@
   (cond ((null? l) (quote ()))
         (else
           (cons (car (car l)) (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((eq? (car l) old) (cons (car l) (cons new (cdr l))))
+          (else (cons (car l) (insertR new old (cdr l)))))))
+
+
+(define insertL
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((eq? (car l) old) (cons new l))
+          (else (cons (car l) (insertL new old (cdr l)))))))
+
+(define subst 
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new (cdr lat)))
+          (else (cons (car lat) (subst new old (cdr lat)))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond ((null? lat) (quote()))
+          ((eq? (car lat) a) (multirember a (cdr lat)))                 
+          (else (cons (car lat) (multirember a (cdr lat)))))))
+
+
+(define multiinsertR
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((eq? (car l) old) (cons (car l) (cons new (multiinsertR new old (cdr l)))))
+          (else (cons (car l) (multiinsertR new old (cdr l)))))))
+
+
+(define multisubst 
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) old) (cons new (multisubst  new old (cdr lat))))
+          (else (cons (car lat) (multisubst new old (cdr lat)))))))
+
+
+(define multiinsertL
+  (lambda (new old l)
+    (cond ((null? l) '())
+          ((eq? (car l) old) (cons new (cons (car l) (multiinsertL new old (cdr l)))))
+          (else (cons (car l) (multiinsertL new old (cdr l)))))))
+
+; Chapter 4 The Little Schemer
