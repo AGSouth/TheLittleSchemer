485dd8150da057881b4b8fb12938f181018efbc4
Joe Maline <joe.maline@applegrovesouth.com>
2020-01-21 16:00:06 -0500

    Chapter 5  rember* function completed

--- TheLittleSchemer.rkt
+++ TheLittleSchemer.rkt
@@ -218,3 +218,11 @@
      (= 0 (sub1 n))))
 
 ; Chapter 5 begins
+
+(define rember*
+  (lambda (a l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+                    (cond ((eq? a (car l)) (rember* a (cdr l)))
+                          (else (cons (car l) (rember* a (cdr l))))))
+          (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
