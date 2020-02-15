be79044df27fba01279f6595d1ac970bcfcaeab8
Joe Maline <joe.maline@applegrovesouth.com>
2020-02-15 11:36:48 -0500

    completed multiinertLR

--- TheLittleSchemer.rkt
+++ TheLittleSchemer.rkt
@@ -688,4 +688,18 @@
  (define last-friend
    (lambda (x y)
      (length x)))
- 
+
+(define multiinsertLR
+   (lambda (new oldL oldR lat)
+     (cond
+       ((null? lat) (quote ()))
+       ((eq? (car lat) oldL)
+         (cons new
+               (cons oldL
+                     (multiinsertLR new oldL oldR (cdr lat)))))
+       ((eq? (car lat) oldR)
+         (cons oldR
+               (cons new
+                     (multiinsertLR new oldL oldR (cdr lat)))))
+       (else (cons (car lat)
+                     (multiinsertLR new oldL oldR (cdr lat)))))))
